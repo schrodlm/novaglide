9f9bf3bb81d679049b2fe4de5a05fdaafdd9a918
Matěj Schrödl <schromat@fit.cvut.cz>
2023-10-14 23:07:01 +0200

    ball/player as sprites

--- src/ball.py
+++ src/ball.py
@@ -4,23 +4,48 @@ import settings
 
 # --------------------------BALL-----------------------------------
 
-class Ball:
+class Ball(pygame.sprite.Sprite):
+
     def __init__(self, x, y):
-        self.position = Vector2(x, y)
+        pygame.sprite.Sprite.__init__(self)
+
+        #radius
+        self.radius = 20
+        self.x, self.y = x,y
+
+
+        self.rect = pygame.Rect(x - self.radius, y - self.radius,
+                                2 * self.radius, 2 * self.radius)
+        
+        self.image = pygame.Surface((2 * self.radius, 2 * self.radius)
+                                    , pygame.SRCALPHA)  # make it transparent
+        
+        self.image = self.image.convert_alpha()
+
+        pygame.draw.circle(self.image, "black",
+            (self.radius, self.radius), self.radius)
+
         self.speed = Vector2(0, 0)
-        self.size = 20
-        print(self.position.x)
-        print(self.position.y)
 
-    def move(self):
-        self.position += self.speed
+    def update(self):
+        #updating position based on speed
+        self.x += self.speed.x
+        self.y += self.speed.y
+        #slowing down the ball
+        self.speed.x /= 1.003
+        self.speed.y /= 1.003
+        self.checkAndHandleRebound()
+        self.setRect()
 
-    def draw(self):
-        pygame.draw.circle(settings.SCREEN, "blue", self.position, self.size)
+    
+    def setRect(self):
+        self.rect = pygame.Rect(self.x - self.radius, self.y - self.radius,
+                                2 * self.radius, 2 * self.radius)
+
 
     def checkAndHandleRebound(self):
-        if self.position.x <= 0 or self.position.x >= settings.WIDTH:
+        if self.x <= 0 or self.x >= settings.WIDTH:
             self.speed.x = -self.speed.x
-        if self.position.y <= 0 or self.position.y >= settings.HEIGHT:
+        if self.y <= 0 or self.y >= settings.HEIGHT:
             self.speed.y = -self.speed.y
 
--- src/player.py
+++ src/player.py
@@ -3,21 +3,27 @@ from pygame import Vector2
 import settings
 
 # --------------------------PLAYER-----------------------------------
-class Player:
-    def __init__(self, x, y):
-        self.position = Vector2(x, y)
-        self.size = 40
-        self.future_position = Vector2(x, y)
 
-    def move(self, dx, dy):
-        self.future_position += dx,dy
-        if self.future_position.x > 0 and self.future_position.x < settings.WIDTH and self.future_position.y > 0 and self.future_position.y < settings.HEIGHT:
-            self.position += dx,dy
-            
-        self.future_position = self.position.copy()
-            
-    def draw(self):
-        pygame.draw.circle(settings.SCREEN, "red", self.position, self.size)
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self,x,y):
+        pygame.sprite.Sprite.__init__(self)
+
+        # width and height
+        self.radius = 40
+        self.x, self.y = x, y
+
+        self.rect = pygame.Rect(x - self.radius, y - self.radius,
+                                2 * self.radius, 2 * self.radius)
+
+        self.image = pygame.Surface((2 * self.radius, 2 * self.radius)
+                                    , pygame.SRCALPHA)  # make it transparent
+        
+        self.image = self.image.convert_alpha()
+        
+        pygame.draw.circle(self.image, "red",
+            (self.radius, self.radius), self.radius)
+
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -29,3 +35,22 @@ class Player:
             self.move(-300*dt, 0)
         if keys[pygame.K_d]:
             self.move(300*dt, 0)
+
+    #TODO: vyresit poradne boundaries
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
+        if self.x < 0:
+            self.x = settings.WIDTH
+        elif self.x > settings.WIDTH:
+            self. x = 0
+        if self.y < 0:
+            self.y = settings.HEIGHT
+        elif self.y > settings.HEIGHT:
+            self.y = 0
+        self.setRect()        
+
+    #TODO: poradne vyresit jak se bude nastavovat pozice
+    def setRect(self):
+        self.rect = pygame.Rect(self.x - self.radius, self.y - self.radius,
+                                2 * self.radius, 2 * self.radius)
