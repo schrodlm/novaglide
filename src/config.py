7b5059b9a55ce11651c6d2939756939e4b98ef31
Tomáš Barhoň <tomas.barhon@hotmail.cz>
2023-12-28 16:49:07 +0100

    Fixing Config constructor

--- src/config.py
+++ src/config.py
@@ -23,33 +23,10 @@ class Config:
     """
     def __init__(self):
         try:
-            with (open("./../settings/config.yaml", 'r',encoding="UTF-8")
-                as file):
+            with open("./../settings/config.yaml", 'r',encoding="UTF-8") as file:
                 self.config = yaml.safe_load(file)
         except:
             raise FileNotFoundError("Failed to load or parse the config file.")
-       
-    def __getitem__(self,key: str) -> Any:
-        """
-        Returns the value associated with the specified key 
-        from the configuration settings.
-
-        Parameters
-        ----------
-        key : str
-            The key for retrieving a specific configuration setting.
-
-        Returns
-        -------
-        Any
-            The value associated with the specified key.
-
-        Notes
-        -----
-        This method allows accessing configuration settings using square bracket notation (e.g., config['key']).
-        """
-        #ensuring by name indexing
-        return self.config[key]
     
 if __name__ == "__main__":
     raise RuntimeError("This module is designed for import only.")
